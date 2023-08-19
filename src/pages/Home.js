c71a807aaebdd152425f43f6727dc8c66a985cc7
Naveen Parne <parneabc@gmail.com>
2023-08-19 19:42:44 +0530

    apipush

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,50 +1,28 @@
-import React from "react";
+import React,{ useState, useEffect} from "react";
 import CareerDetailCard from "../components/CareerDetailCard";
 
 
-const careerData = [
-    {
-      title: 'Job Title 1',
-      qualifications: 'Degree in XYZ field',
-      pay: '50,000 - 60,000',
-      totalPosts: 10,
-      exam: 'Written and Interview',
-    },
 
-    {
-        title: 'Job Title 2',
-        qualifications: 'Degree in ABC field',
-        pay: '20,000 - 30,000',
-        totalPosts: 100,
-        exam: 'Written and Interview',
-      },
-
-      {
-        title: 'Job Title 3',
-        qualifications: 'Degree in ABC field',
-        pay: '20,000 - 30,000',
-        totalPosts: 100,
-        exam: 'Written and Interview',
-      },
-      {
-        title: 'Job Title 4',
-        qualifications: 'Degree in ABC field',
-        pay: '20,000 - 30,000',
-        totalPosts: 100,
-        exam: 'Written and Interview',
-      },
-
-      {
-        title: 'Job Title 5',
-        qualifications: 'Degree in ABC field',
-        pay: '20,000 - 30,000',
-        totalPosts: 100,
-        exam: 'Written and Interview',
-      },
-    // Add more career objects here
-  ];
 
 function Home () {
+
+ const [careerData, setCareerData] = useState([]);
+
+  useEffect(() => {
+    async function fetchCareerData() {
+       try {
+        const response = await fetch('https://api.sheety.co/2ec566badc7051e41eb62ad701db5227/theCareerApp/sheet1');
+        const data = await response.json();
+        setCareerData(data.sheet1);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchCareerData();
+   }, [{careerData}]);
+
+
 
     return (
         <div>
